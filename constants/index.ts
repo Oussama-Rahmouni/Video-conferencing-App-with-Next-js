8dbdafb761bcc570aefe2195bd1a533bc6537a84
Oussama-Rahmouni <oussama.rahmouni.manager@gmail.com>
2024-05-12 15:10:43 +0100

    creating constants 29:12

---
+++ constants/index.ts
@@ -0,0 +1,23 @@
+const sidebarLinks = [
+    {
+        label:"home",
+        route:"/",
+        imgUrl:"/icons/something.svg"
+    },
+    {
+        label:"Upcoming",
+        route:"/upcoming",
+        imgUrl:"/icons/upcoming.svg"
+    },
+    {
+        label:"home",
+        route:"/",
+        imgUrl:"/icons/something.svg"
+    },
+    {
+        label:"home",
+        route:"/",
+        imgUrl:"/icons/something.svg"
+    },
+    
+]
